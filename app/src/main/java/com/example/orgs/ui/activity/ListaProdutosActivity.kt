950b90c6103d515c657e8d5ebec0f5bba24d502e
Rafael Leonan <faelleonan@gmail.com>
2023-07-03 20:42:17 -0300

    Refatorando código

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -1,33 +1,44 @@
 package com.example.orgs.ui.activity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
-import com.example.orgs.models.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.math.BigDecimal
 
-class MainActivity: AppCompatActivity(R.layout.activity_main) {
+class ListaProdutosActivity: AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
     }
 
     override fun onResume() {
         super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val dao = ProdutosDao()
-        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+        recyclerView.adapter = adapter
+    }
+
+    private fun configuraFab() {
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
-            val intent = Intent(this, FormularioProdutoActivity::class.java)
-            startActivity(intent)
+            vaiParaFormularioProduto()
         }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
     }
 }
