376885b8569985dfae9cf9e7e24f57399deabd25
Rafael Leonan <faelleonan@gmail.com>
2023-07-03 19:22:40 -0300

    Add new Activity

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.orgs.ui.activity
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
@@ -10,10 +11,9 @@ import com.example.orgs.models.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity: Activity() {
+class MainActivity: AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = "Cesta de frutas"
 //        val descricao = findViewById<TextView>(R.id.descricao)
